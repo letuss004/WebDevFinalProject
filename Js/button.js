8f8b7150196cc16fda95759b0e6c5df9e689c142
ibiann <trungpham152000@gmail.com>
2021-10-24 13:10:43 +0700

    Script for main and login page

---
+++ Js/button.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var un = document.forms["myForm"]["Uname"].value;
+    var pw = document.forms["myForm"]["Pass"].value;
+    if (un == "admin" && pw == "1234") {
+        window.location.href="/index2.html";
+    } else {
+        alert("Invalid UserName and Password");
+    }
+}
