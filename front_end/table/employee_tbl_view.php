99a7273a90d9b86c413b443498ba23cfd8822a03
letuss004 <letuss004@gmail.com>
2021-10-27 23:23:07 +0700

    add validate, ...

--- front_end/table/employee_tbl_view.php
+++ front_end/table/employee_tbl_view.php
@@ -138,6 +138,7 @@ session_start();
             echo "</tr>";
         } ?>
         </tbody>
+
     </table>
 </div>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
